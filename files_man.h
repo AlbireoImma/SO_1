97ecda515257ec123dd6a9871cccdaaaf92ab42c
Francisco <francisco@ubuntu.ubuntu-domain>
2018-09-27 21:56:21 -0400

    Creacion organizar()

--- files_man.h
+++ files_man.h
@@ -73,11 +73,13 @@ void print_pel(peliculas *pel){
 
 void crear_dir(char *genero,char *anio,char *nombre_archivo){
         char path[700] = "";
+  #ifdef __linux__
         strncat(path,genero,strlen(genero));
+        mkdir(path, 0755);
         strncat(path,"/",1);
         strncat(path,anio,strlen(anio));
-  #ifdef __linux__
-        mkdir(path, 0777);
+        mkdir(path, 0755);
+        strncat(path,"/",1);
         strncat(path,nombre_archivo,strlen(nombre_archivo));
         rename(nombre_archivo,path);
   #else
@@ -87,5 +89,24 @@ void crear_dir(char *genero,char *anio,char *nombre_archivo){
   #endif
 }
 
-void crear_generos(){
+int organizar(peliculas *pel){
+        int i;
+        FILE *file;
+        char genero[250];
+        char anio[250];
+        for (i = 0; i < pel->cant_peliculas; i++) {
+                //printf("Abriendo: %s\n",pel->peliculas[i]);
+                file = fopen(pel->peliculas[i],"r");
+                if (file == NULL) {
+                        return -1;
+                }
+                //printf("Archivo abierto\n");
+                fscanf(file, "%s",genero);
+                //printf("genero --> %s\n",genero );
+                fscanf(file, "%s",anio);
+                //printf("anio --> %s\n",anio );
+                fclose(file);
+                crear_dir(genero,anio,pel->peliculas[i]);
+        }
+        return 1;
 }
