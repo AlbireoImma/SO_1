680af8d7eef7fb51b496075203c229fba800010b
Francisco <francisco@ubuntu.ubuntu-domain>
2018-09-27 21:56:00 -0400

    Limpieza Codigo

--- test.c
+++ test.c
@@ -4,29 +4,20 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include "files_man.h"
+#include <time.h>
 
 int main(void) {
+        clock_t begin = clock();
         peliculas *pel = obtener_peliculas(".");
         printf("Cantidad de archivos en Raiz: %d\n", pel->cant_peliculas);
-        print_pel(pel);
-        int i;
-        FILE *file;
-        char genero[250];
-        char anio[250];
-        for (i = 0; i < pel->cant_peliculas; i++) {
-                printf("Abriendo: %s\n",pel->peliculas[i]);
-                file = fopen(pel->peliculas[i],"r");
-                if (file =NULL) {
-                        printf("Mal abierto\n");
-                }
-                printf("Archivo abierto\n");
-                fscanf(file, "%s",genero);
-                printf("1\n");
-                printf("genero --> %s\n",genero );
-                fscanf(file, "%s",&anio);
-                printf("anio --> %s\n",anio );
-                fclose(file);
-                crear_dir(genero,anio,pel->peliculas[i]);
+        int Flag = pel->cant_peliculas;
+        while (Flag > 0) {
+          pel = obtener_peliculas(".");
+          organizar(pel);
+          Flag = pel->cant_peliculas;
         }
+        clock_t end = clock();
+        double time_s = (double)(end - begin) / CLOCKS_PER_SEC;
+        printf("Archivos organizados en: %lf segundos\n",time_s);
         return 0;
 }
