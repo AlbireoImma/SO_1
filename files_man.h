2e9c9a6cfa4d47651025545877472b48954aaf3e
Francisco <francisco@ubuntu.ubuntu-domain>
2018-09-27 20:52:35 -0400

    Beautify de files_man

--- files_man.h
+++ files_man.h
@@ -6,84 +6,86 @@
 
 typedef struct
 {
-  char **peliculas;
-  int cant_peliculas;
+        char **peliculas;
+        int cant_peliculas;
 } peliculas;
 
 peliculas *obtener_peliculas(char *nombre_dir){
-  int archivos = 0;
-  struct dirent *de;
-  DIR *dir = opendir(nombre_dir);
+        int archivos = 0;
+        struct dirent *de;
+        DIR *dir = opendir(nombre_dir);
 
-  if (dir == NULL){  // Error al abrir el directorio
-    peliculas *pel = (peliculas *)calloc(1,sizeof(peliculas));
-    pel->cant_peliculas = -1;
-    return pel;
-  }
+        if (dir == NULL) {
+                // Error al abrir el directorio
+                peliculas *pel = (peliculas *)calloc(1,sizeof(peliculas));
+                pel->cant_peliculas = -1;
+                return pel;
+        }
 
-  while ((de = readdir(dir)) != NULL){
-    // Que no sea ninguno de los archivos usados por el programa
-    if (strcmp(de->d_name,"test.c") == 0 || strcmp(de->d_name,"test") == 0 || strcmp(de->d_name,"files_man.h") == 0) {
-      continue;
-    } else {
-      if (de->d_type == 8) {
-        archivos++;
-      }
-    }
-  }
-  closedir(dir);
-  peliculas *pel = (peliculas *)calloc(1,sizeof(peliculas));
-  pel->peliculas = (char **)calloc(archivos,sizeof(char*));
-  pel->cant_peliculas = archivos;
-  int i;
-  for (i = 0; i < archivos; i++) {
-    pel->peliculas[i] = (char *)calloc(251,sizeof(char));
-  }
-  dir = opendir(nombre_dir);
+        while ((de = readdir(dir)) != NULL) {
+                // Que no sea ninguno de los archivos usados por el programa
+                if (strcmp(de->d_name,"test.c") == 0 || strcmp(de->d_name,"test") == 0 || strcmp(de->d_name,"files_man.h") == 0) {
+                        continue;
+                } else {
+                        if (de->d_type == 8) {
+                                archivos++;
+                        }
+                }
+        }
+        closedir(dir);
+        peliculas *pel = (peliculas *)calloc(1,sizeof(peliculas));
+        pel->peliculas = (char **)calloc(archivos,sizeof(char*));
+        pel->cant_peliculas = archivos;
+        int i;
+        for (i = 0; i < archivos; i++) {
+                pel->peliculas[i] = (char *)calloc(251,sizeof(char));
+        }
+        dir = opendir(nombre_dir);
 
-  if (dir == NULL){  // Error al abrir el directorio
-    peliculas *pel = (peliculas *)calloc(1,sizeof(peliculas));
-    pel->cant_peliculas = -1;
-    return pel;
-  }
-  i = 0;
-  while ((de = readdir(dir)) != NULL){
-    // Que no sea ninguno de los archivos usados por el programa
-    if (strcmp(de->d_name,"test.c") == 0 || strcmp(de->d_name,"test") == 0 || strcmp(de->d_name,"files_man.h") == 0) {
-      continue;
-    } else {
-      if (de->d_type == 8) {
-        pel->peliculas[i]=de->d_name;
-        i++;
-      }
-    }
-  }
-  closedir(dir);
-  return pel;
+        if (dir == NULL) { // Error al abrir el directorio
+                peliculas *pel = (peliculas *)calloc(1,sizeof(peliculas));
+                pel->cant_peliculas = -1;
+                return pel;
+        }
+        i = 0;
+        while ((de = readdir(dir)) != NULL) {
+                // Que no sea ninguno de los archivos usados por el programa
+                if (strcmp(de->d_name,"test.c") == 0 || strcmp(de->d_name,"test") == 0 || strcmp(de->d_name,"files_man.h") == 0) {
+                        continue;
+                } else {
+                        if (de->d_type == 8) {
+                                pel->peliculas[i]=de->d_name;
+                                i++;
+                        }
+                }
+        }
+        closedir(dir);
+        return pel;
 }
 
 void print_pel(peliculas *pel){
-  int i;
-  for (i = 0; i < pel->cant_peliculas; i++) {
-    printf("%d) %s\n",i+1,pel->peliculas[i]);
-  }
-  return;
+        int i;
+        for (i = 0; i < pel->cant_peliculas; i++) {
+                printf("%d) %s\n",i+1,pel->peliculas[i]);
+        }
+        return;
 }
 
 void crear_dir(char *genero,char *anio,char *nombre_archivo){
-  char path[700] = "";
-  strncat(path,genero,strlen(genero));
-  strncat(path,"/",1);
-  strncat(path,anio,strlen(anio));
+        char path[700] = "";
+        strncat(path,genero,strlen(genero));
+        strncat(path,"/",1);
+        strncat(path,anio,strlen(anio));
   #ifdef __linux__
-    mkdir(path, 0777);
-    strncat(path,nombre_archivo,strlen(nombre_archivo));
-    rename(nombre_archivo,path);
+        mkdir(path, 0777);
+        strncat(path,nombre_archivo,strlen(nombre_archivo));
+        rename(nombre_archivo,path);
   #else
-    _mkdir(path);
-    strncat(path,nombre_archivo,strlen(nombre_archivo));
-    _rename(nombre_archivo,path);
+        _mkdir(path);
+        strncat(path,nombre_archivo,strlen(nombre_archivo));
+        _rename(nombre_archivo,path);
   #endif
 }
 
-void crear_generos(){}
+void crear_generos(){
+}
